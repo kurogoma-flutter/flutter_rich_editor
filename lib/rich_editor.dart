ab7ffa2636823fe3c142cb6bbb63d952df263381
Kurogoma939 <kan.sss.939@gmail.com>
2023-02-26 03:09:10 +0900

    不要なSizedBox削除

--- lib/rich_editor.dart
+++ lib/rich_editor.dart
@@ -20,35 +20,31 @@ class _RichTextEditorState extends State<RichTextEditor> {
           border: Border.all(color: Colors.grey),
           borderRadius: BorderRadius.circular(4),
         ),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.6,
-          height: MediaQuery.of(context).size.height * 0.6,
-          child: Column(
-            children: [
-              quill.QuillToolbar.basic(controller: _controller),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 8),
-                child: Divider(
-                  height: 1,
-                  color: Colors.grey,
-                ),
+        child: Column(
+          children: [
+            quill.QuillToolbar.basic(controller: _controller),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Divider(
+                height: 1,
+                color: Colors.grey,
               ),
-              Expanded(
-                child: quill.QuillEditor(
-                  controller: _controller,
-                  scrollController: ScrollController(),
-                  scrollable: true,
-                  focusNode: FocusNode(),
-                  autoFocus: true,
-                  readOnly: false,
-                  expands: false,
-                  padding: const EdgeInsets.all(8),
-                  enableInteractiveSelection: true,
-                  showCursor: true,
-                ),
+            ),
+            Expanded(
+              child: quill.QuillEditor(
+                controller: _controller,
+                scrollController: ScrollController(),
+                scrollable: true,
+                focusNode: FocusNode(),
+                autoFocus: true,
+                readOnly: false,
+                expands: false,
+                padding: const EdgeInsets.all(8),
+                enableInteractiveSelection: true,
+                showCursor: true,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
